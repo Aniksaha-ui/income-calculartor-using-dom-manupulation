c53066b64f25ad0960d7ad7b4a635f3ed0b38599
Aniksaha-ui <sahaanik1045@gmail.com>
2022-02-17 01:30:02 +0600

    total expenses and balance statement clear with error checking

--- js/app.js
+++ js/app.js
@@ -1,12 +1,50 @@
 let calculateButton = document.getElementById("calculate");
 let saveButton = document.getElementById("save");
+//get income input
+function getIncome() {
+  let incomeField = document.getElementById("income-input");
+  let income = incomeField.value;
+  return income;
+}
+
+//get expenses inputs
+
+function getIndiviualCosting(expensesArea) {
+  var inputCostingField = document.getElementById(expensesArea + "-cost");
+  var cost = parseFloat(inputCostingField.value);
+  if (isNaN(cost)) {
+    alert("Please Enter a number");
+  } else if (cost < 0) {
+    alert("Money Can not be negative");
+  } else {
+    return cost;
+  }
+}
+
+//calculate Balance
+function balanceCalculate(totalCosting) {
+  var income = getIncome();
+  if (income < totalCosting) {
+    alert("Have not sufficent money!");
+  } else {
+    var totalExpensesField = document.getElementById("expenses");
+    totalExpensesField.innerText = totalCosting;
+    var remainBalance = income - totalCosting;
+    var balanceField = document.getElementById("balance");
+    balanceField.innerText = remainBalance;
+  }
+}
+
 //when calculate button clicked
 calculateButton.addEventListener("click", function () {
-  console.log("calculate button clicked");
+  let income = getIncome();
+  let foodCost = getIndiviualCosting("food");
+  let rentCost = getIndiviualCosting("rent");
+  let clothCost = getIndiviualCosting("cloth");
+  let totalCosting = foodCost + rentCost + clothCost;
+  let calculateBalance = balanceCalculate(totalCosting);
 });
 
-//when save button clicked
-
 saveButton.addEventListener("click", function () {
-  console.log("save button clicked");
+  console.log("Save Button Clicked");
 });
