fdbc72e8fea8bf9022382e20bc27c3934c1025e4
Aniksaha-ui <sahaanik1045@gmail.com>
2022-02-17 00:50:06 +0600

    given ids all field and button clicked is working

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let calculateButton = document.getElementById("calculate");
+let saveButton = document.getElementById("save");
+//when calculate button clicked
+calculateButton.addEventListener("click", function () {
+  console.log("calculate button clicked");
+});
+
+//when save button clicked
+
+saveButton.addEventListener("click", function () {
+  console.log("save button clicked");
+});
